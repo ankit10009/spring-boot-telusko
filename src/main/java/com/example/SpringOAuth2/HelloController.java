d997a7d86836a70524181980f99f8a6007300b5d
ankit10009 <ti.ankit1@gmail.com>
2024-08-20 11:41:55 +0530

    Initial version

---
+++ src/main/java/com/example/SpringOAuth2/HelloController.java
@@ -0,0 +1,12 @@
+package com.example.SpringOAuth2;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String greet(){
+        return "Welcome to Telusko, telusko.com";
+    }
+}
